ecb0e5fee4703c3e638be6e9727d6874b2da6a9a
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-09-20 11:17:25 -0600

    set up db files

---
+++ db/migrate/20210920171145_create_subscriptions.rb
@@ -0,0 +1,14 @@
+class CreateSubscriptions < ActiveRecord::Migration[6.1]
+  def change
+    create_table :subscriptions do |t|
+      t.string :title
+      t.float :price
+      t.integer :status
+      t.string :frequency
+      t.references :user, null: false, foreign_key: true
+      t.references :tea, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
