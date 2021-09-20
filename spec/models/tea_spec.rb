1a45057afe7afd72ead94cec090ec651e6dcf3f9
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-09-20 14:38:27 -0600

    create initial model tests

--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Tea, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    [:variety, :description, :temperature, :brew_time].each do |attribute|
+      it { should validate_presence_of attribute }
+    end
+
+    it { should validate_numericality_of(:temperature).only_integer }
+    it { should validate_numericality_of(:temperature).is_greater_than_or_equal_to(-50) }
+    it { should validate_numericality_of(:temperature).is_less_than_or_equal_to(150) }
+  end
+
+  describe "relationships" do
+    it { should have_many :subscriptions }
+  end
 end
