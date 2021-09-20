d13975a6f64c8a5ac0ba49407baffbcee6c4361f
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-09-20 14:38:47 -0600

    implement models for validations and relationships

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,2 +1,10 @@
 class Tea < ApplicationRecord
+  validates :variety, :description, :temperature, :brew_time, presence: true
+  validates :temperature, numericality: { 
+    only_integer: true,
+    greater_than_or_equal_to: -50,
+    less_than_or_equal_to: 150
+  }
+
+  has_many :subscriptions
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :full_name, :email, :address, presence: true
+  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  has_many :subscriptions
 end
