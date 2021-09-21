9aaf408725aa6326c760050dcbd023628a548cc7
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-09-21 14:28:24 -0600

    modify tea faker data

--- spec/factories/teas.rb
+++ spec/factories/teas.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     variety { Faker::Tea.variety }
     description { Faker::Movies::StarWars.quote }
     temperature { Faker::Number.within(range: -50..150) }
-    brew_time { Faker::Time.backward(days: 2, period: :morning, format: :short)[-1..-5] }
+    brew_time { Faker::Time.backward(days: 2, period: :morning, format: :short) }
   end
 end
