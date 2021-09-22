23b3344c741b60a77c42508acc37a6feb30c26b2
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-09-22 14:58:34 -0600

    implement post create route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       namespace :users do
         scope '/:id', :as => 'user' do
-          resources :subscriptions, only: :index
+          resources :subscriptions, only: [:index, :create]
         end
       end
     end
