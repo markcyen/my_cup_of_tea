d80c1e39b48864a771ea7a617a6110725b174662
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-09-22 16:00:40 -0600

    create update action

--- app/controllers/api/v1/users/subscriptions_controller.rb
+++ app/controllers/api/v1/users/subscriptions_controller.rb
@@ -1,17 +1,30 @@
 class Api::V1::Users::SubscriptionsController < ApplicationController
   def index
-    user = User.find(params[:id])
+    user = User.find(params[:user_id])
     user_subscriptions = SubscriptionsData.new(user.subscriptions)
     render json: SubscriptionSerializer.subscriptions_list(user, user_subscriptions), status: :ok
   end
 
   def create
     begin
-      user = User.find(params[:id])
+      user = User.find(params[:user_id])
       tea = Tea.find(params[:tea_id])
       new_subscription = user.subscriptions.create(new_subscription_params)
 
       render json: SubscriptionSerializer.activate(user, tea, new_subscription), status: :created
+    rescue ActiveRecord::RecordNotFound
+      render json: {
+        error: 'Bad request!',
+        status: 'Failed!'
+      }, status: :bad_request
+    end
+  end
+
+  def update
+    begin
+      subscription = Subscription.find(params[:id])
+      subscription.update(new_subscription_params)
+      render json: SubscriptionSerializer.new(subscription), status: :ok
     rescue ActiveRecord::RecordNotFound
       render json: {
         error: 'Bad request!',
