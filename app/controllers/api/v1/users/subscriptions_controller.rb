ddffb668afa592c4a4e85649f67e07563605b3e9
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-09-22 14:59:04 -0600

    implement controller create action

--- app/controllers/api/v1/users/subscriptions_controller.rb
+++ app/controllers/api/v1/users/subscriptions_controller.rb
@@ -2,6 +2,27 @@ class Api::V1::Users::SubscriptionsController < ApplicationController
   def index
     user = User.find(params[:id])
     user_subscriptions = SubscriptionsData.new(user.subscriptions)
-    render json: SubscriptionSerializer.subscriptions_list(user, user_subscriptions)
+    render json: SubscriptionSerializer.subscriptions_list(user, user_subscriptions), status: :ok
+  end
+
+  def create
+    begin
+      user = User.find(params[:id])
+      tea = Tea.find(params[:tea_id])
+      new_subscription = user.subscriptions.create(new_subscription_params)
+
+      render json: SubscriptionSerializer.activate(user, tea, new_subscription), status: :created
+    rescue ActiveRecord::RecordNotFound
+      render json: {
+        error: 'Bad request!',
+        status: 'Failed!'
+      }, status: :bad_request
+    end
+  end
+
+  private
+
+  def new_subscription_params
+    params.permit(:title, :price, :frequency, :status, :tea_id)
   end
 end
