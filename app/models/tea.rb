cef58b0dda9f30d1f12d49900a405b8f8057ff36
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-09-22 19:06:11 -0600

    modify column name

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,5 +1,5 @@
 class Tea < ApplicationRecord
-  validates :variety, :description, :temperature, :brew_time, presence: true
+  validates :variety, :description, :temperature, :brew_time_in_min, presence: true
   validates :temperature, numericality: { 
     only_integer: true,
     greater_than_or_equal_to: -50,
